60b692c0c1c8dd35991594a799af6557aca1e0f7
Ganesh <gtiwari333@gmail.com>
2019-11-28 11:36:44 -0600

    diff utility

---
+++ README.md
@@ -0,0 +1,9 @@
+# A simple, yet powerful diff generator and viewer utility.
+
+Built with Spring Boot, java-diff-utils and diff2html libraries
+
+### How to Run
+
+- Download the project
+- Run `./mvnw package` OR `mvn package`
+- Run `java -jar target/diff-v1.jar`
---
+++ src/main/java/diff/ManualReviewApp.java
@@ -0,0 +1,54 @@
+package diff;
+
+import com.github.difflib.DiffUtils;
+import com.github.difflib.UnifiedDiffUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Arrays;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.ResponseEntity.ok;
+
+@SpringBootApplication
+public class ManualReviewApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ManualReviewApp.class, args);
+    }
+}
+
+@Controller
+class DiffController {
+
+    @GetMapping({"/", ""})
+    String getHome() {
+        return "redirect:index.html";
+    }
+
+    @PostMapping(value = "/diff")
+    ResponseEntity<String> doDiff(@RequestBody DiffRequest req) throws Exception {
+        return ok(getDiffStr(req.left, req.right, "Diff"));
+    }
+
+    static class DiffRequest {
+        public String left;
+        public String right;
+    }
+
+    String getDiffStr(String left, String right, String label) throws Exception {
+        //left is the reference
+        var originalLines = Arrays.stream(left.split("\n")).collect(toList());
+
+        var patch = DiffUtils.diff(left, right, null);
+
+        var diffStrList = UnifiedDiffUtils.generateUnifiedDiff(label, label, originalLines, patch, 10);
+
+        return String.join("\n", diffStrList);
+    }
+
+}
